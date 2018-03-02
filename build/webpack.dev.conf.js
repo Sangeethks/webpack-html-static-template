0c36db41a84e00993bd4eef67fbbb1ed00d2e1c7
Sangeethks <sangeethks.in@gmail.com>
2018-03-02 12:24:15 +0530

    PROJECT DELETE - Starting from scratch

--- build/webpack.dev.conf.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const fs = require('fs')
-const path = require('path')
-const utils = require('./utils')
-const webpack = require('webpack')
-const config = require('../config')
-const merge = require('webpack-merge')
